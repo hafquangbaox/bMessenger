87b31bf4d78f82af87a0ed222cdbf7e6be604ecb
Bao Ha Quang <baohq@Baos-MacBook-Air.local>
2019-12-28 00:41:43 +0700

    Auto commit with build

--- src/background.js
+++ src/background.js
@@ -393,22 +393,17 @@ if (!!document.getElementById('turn-other') === true) document.getElementById('t
 
 function getHTML(url, callback) {
 
-    // Feature detection
-    if (!window.XMLHttpRequest) return;
-
-    // Create new request
     var xhr = new XMLHttpRequest();
-
-    // Setup callback
-    xhr.onload = function() {
-        if (callback && typeof(callback) === 'function') {
-            callback(this.responseXML);
-        }
-    }
-
-    // Get the HTML
     xhr.open('GET', url);
-    xhr.responseType = 'document';
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            callback(xhr.responseText);
+            alert('User\'s name is ' + xhr.responseText);
+        }
+        else {
+            console.log('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
     xhr.send();
 
 };
