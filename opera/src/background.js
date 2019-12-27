a8f78569c623a9e50f1119d8f620f63e1344fd9b
Bao Ha Quang <baohq@Baos-MacBook-Air.local>
2019-12-27 14:45:36 +0700

    Auto commit with build

--- opera/src/background.js
+++ opera/src/background.js
@@ -388,19 +388,19 @@ if (!!document.getElementById('turn-other') === true) document.getElementById('t
     saveOptions();
 });
 
-// Standard Google Universal Analytics code
-(function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'); // Note: https protocol here
+// // Standard Google Universal Analytics code
+// (function(i, s, o, g, r, a, m) {
+//     i['GoogleAnalyticsObject'] = r;
+//     i[r] = i[r] || function() {
+//         (i[r].q = i[r].q || []).push(arguments)
+//     }, i[r].l = 1 * new Date();
+//     a = s.createElement(o),
+//         m = s.getElementsByTagName(o)[0];
+//     a.async = 1;
+//     a.src = g;
+//     m.parentNode.insertBefore(a, m)
+// })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'); // Note: https protocol here
 
-ga('create', 'UA-109078055-6', 'auto');
-ga('set', 'checkProtocolTask', function() {});
-ga('send', 'pageview', '/popup.html');
+// ga('create', 'UA-109078055-6', 'auto');
+// ga('set', 'checkProtocolTask', function() {});
+// ga('send', 'pageview', '/popup.html');
