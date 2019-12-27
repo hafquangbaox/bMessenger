c0f689a6a3f69c642cd069ba18c525060d0194fe
Bao Ha Quang <baohq@Baos-MacBook-Air.local>
2019-12-27 23:58:28 +0700

    Auto commit with build

--- firefox/src/content.js
+++ firefox/src/content.js
@@ -712,7 +712,8 @@ var selector = {
                     selector: 'div.fbNubFlyoutOuter>div.fbNubFlyoutInner>div:nth-child(2)>div>div>div>div:nth-child(2)>div>div:nth-child(1)>div>div>div>div>div>div>div.direction_ltr>div[data-testid="attachment_root"]>span:nth-child(1)>div>div>a>div>div>div',
                     inline: 'div.fbNubFlyoutOuter>div.fbNubFlyoutInner>div:nth-child(2)>div>div>div>div:nth-child(2)>div>div:nth-child(1)>div>div',
                     inlineSelector: 'div>div>div>div>div.direction_ltr>div[data-testid="attachment_root"]>span:nth-child(1)>div>div>a>div>div>div',
-                    block: 'div.fbDockChatTabFlyout'
+                    block: 'div.fbDockChatTabFlyout',
+                    rightAlign: true
                 }
                 , {
                     isComponent: "message",
