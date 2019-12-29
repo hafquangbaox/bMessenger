160fd6daa683333d36ae5f9245e0ddc11e7586b2
Bao Ha Quang <baohq@Baos-MacBook-Air.local>
2019-12-29 22:00:05 +0700

    Auto commit with build

--- chrome/src/content.js
+++ chrome/src/content.js
@@ -1012,6 +1012,7 @@ function httpGet(theUrl, callback) {
     }
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log(xmlhttp.responseText);
             callback(xmlhttp.responseText);
         } else {
             callback(null);
