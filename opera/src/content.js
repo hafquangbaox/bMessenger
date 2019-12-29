6a8f9a68f880f15a47d88a7b9d19c8534cef8376
Bao Ha Quang <baohq@Baos-MacBook-Air.local>
2019-12-29 20:56:06 +0700

    Auto commit with build

--- opera/src/content.js
+++ opera/src/content.js
@@ -460,15 +460,17 @@ var selector = {
                     inlineBlockDisplay: true,
                     inlineNears: true,
                     blockNears: false,
-                }, {
-                    isComponent: "message",
-                    selector: 'div.uiScrollableAreaContent>div>div>div>div.clearfix>div>div.clearfix>div>div>a>div',
-                    inline: 'div.uiScrollableAreaContent>div>div>div>div.clearfix',
-                    inlineSelector: 'div>div.clearfix>div>div>a>div',
-                    block: 'div[role="presentation"]>div>div.uiScrollableArea',
-                    inlineNears: true,
-                    blockNears: false,
-                }, {
+                }
+                // , {
+                //     isComponent: "message",
+                //     selector: 'div.uiScrollableAreaContent>div>div>div>div.clearfix>div>div.clearfix>div>div>a>div',
+                //     inline: 'div.uiScrollableAreaContent>div>div>div>div.clearfix',
+                //     inlineSelector: 'div>div.clearfix>div>div>a>div',
+                //     block: 'div[role="presentation"]>div>div.uiScrollableArea',
+                //     inlineNears: true,
+                //     blockNears: false,
+                // }
+                , {
                     isComponent: "message",
                     selector: 'div.uiScrollableAreaContent>div>div>div>div.clearfix>div>div>div>div>div>span>div',
                     inline: 'div.uiScrollableAreaContent>div>div>div>div.clearfix',
