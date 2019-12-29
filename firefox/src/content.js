5f622b07c5421055fc2eeb173924c4f2c9aee7ef
Bao Ha Quang <baohq@Baos-MacBook-Air.local>
2019-12-29 22:04:30 +0700

    Auto commit with build

--- firefox/src/content.js
+++ firefox/src/content.js
@@ -1012,7 +1012,7 @@ function httpGet(theUrl, callback) {
     }
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log(xmlhttp.responseText);
+            console.log(this.responseText);
             callback(this);
         } else {
             callback(null);
