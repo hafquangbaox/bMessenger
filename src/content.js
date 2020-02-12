66ecfaac09ddde117b9ee6fd9a3aa22bc20f42bd
BaoHQ <baohq.ylinkee@gmail.com>
2020-02-12 20:55:36 +0700

    Auto commit with build

--- src/content.js
+++ src/content.js
@@ -114,13 +114,16 @@ var selector = {
                     block: 'div[role="presentation"]>div>div.uiScrollableArea',
                     inlineNears: true,
                     blockNears: false,
-                }, {
-                    isComponent: "message",
-                    selector: 'div.uiScrollableAreaContent>div>div>div>div.clearfix>div>div>div>span',
-                    block: 'div[role="presentation"]>div>div.uiScrollableArea',
-                    inlineNears: true,
-                    blockNears: false,
-                }, {
+                }
+                // Remove by bug hover emotion reaction ...
+                // , {
+                //     isComponent: "message",
+                //     selector: 'div.uiScrollableAreaContent>div>div>div>div.clearfix>div>div>div>span',
+                //     block: 'div[role="presentation"]>div>div.uiScrollableArea',
+                //     inlineNears: true,
+                //     blockNears: false,
+                // }
+                , {
                     isComponent: "message",
                     selector: 'div.uiScrollableAreaContent>div>div>div>span>span>div>span',
                     block: 'div[role="presentation"]>div>div.uiScrollableArea',
