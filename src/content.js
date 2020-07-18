05482f10495027dbbab6144c5a12b79958237586
pdudctnguyen <pdudctnguyen@gmail.com>
2020-07-18 17:04:35 +0700

    Auto commit with build

--- src/content.js
+++ src/content.js
@@ -434,7 +434,17 @@ var selector = {
                     blockSelector: ' div:not(:first-child)>div:first-child a>div:first-child>div:last-child>div:first-child>div>div>div:last-child',
                     inlineNears: true,
                     blockNears: false,
+                },
+
+                {
+                    isComponent: "avatar",
+                    block: 'div[data-pagelet="root"] div[role="banner"] div[data-testid="Keycommand_wrapper_ModalLayer"] div[role="dialog"] div[data-testid="MWJewelThreadListContainer"]',
+                    blockSelector: ' div:not(:first-child)>div:first-child a>div:first-child>div:last-child>div:last-child div[role="grid"] div[role="row"]',
+                    inlineNears: true,
+                    blockNears: false,
                 }
+
+
                 /*Version 2 end*/
                 ],
                 'blur25': [{
