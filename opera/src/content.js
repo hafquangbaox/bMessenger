9360e8a634d6e27c9b60b8b1c9ec43e771eda92d
pdudctnguyen <pdudctnguyen@gmail.com>
2020-07-19 12:57:54 +0700

    Auto commit with build

--- opera/src/content.js
+++ opera/src/content.js
@@ -321,19 +321,35 @@ var selector = {
                 },
                     {
                         isComponent: "avatar",
-                        selector: ' div.uiScrollableArea>div.uiScrollableAreaWrap>div.uiScrollableAreaBody>div.uiScrollableAreaContent div[aria-hidden="true"]>div>div>img',
+                        selector: 'div.uiScrollableArea>div.uiScrollableAreaWrap>div.uiScrollableAreaBody>div.uiScrollableAreaContent div[aria-hidden="true"]>div>div>img',
                         block: 'div[role="main"] span',
                         inlineNears: true,
                         blockNears: false,
                     },
 
                     {
-                    isComponent: "name",
-                    selector: 'div[data-testid="info_panel"]>div>div>div.uiScrollableAreaWrap>div.uiScrollableAreaBody>div.uiScrollableAreaContent>div>div:first-of-type>div:nth-of-type(2)',
-                    block: 'div[role="main"]',
-                    inlineNears: true,
-                    blockNears: false,
-                }, {
+                        isComponent: "name",
+                        selector: 'div[data-testid="info_panel"]>div>div>div.uiScrollableAreaWrap>div.uiScrollableAreaBody>div.uiScrollableAreaContent>div>div:first-of-type>div:nth-of-type(2)',
+                        block: 'div[role="main"]',
+                        inlineNears: true,
+                        blockNears: false,
+                    },
+
+
+
+
+                    {
+                        isComponent: "name",
+                        selector: 'div[data-testid="info_panel"]>div>div>div.uiScrollableAreaWrap>div.uiScrollableAreaBody>div.uiScrollableAreaContent>div>div:first-of-type>div:nth-of-type(2)',
+                        block: 'div[role="main"]',
+                        inlineNears: true,
+                        blockNears: false,
+                    },
+
+
+
+
+                    {
                     isComponent: 'name',
                     selector: 'div.uiScrollableAreaContent>div>div>div>div.clearfix>div>h5',
                     block: 'div[role="presentation"]>div>div.uiScrollableArea',
