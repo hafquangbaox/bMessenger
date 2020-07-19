27f557a7ae7905b07f57b3bf9664fe36507846bc
pdudctnguyen <pdudctnguyen@gmail.com>
2020-07-19 09:09:22 +0700

    Auto commit with build

--- firefox/src/content.js
+++ firefox/src/content.js
@@ -849,14 +849,14 @@ var selector = {
                     {
                         isComponent: "name",
                         block: 'div[data-pagelet="root"] div[data-pagelet="ChatTab"] div[data-visualcompletion="ignore-dynamic"] div[role="region"]>div>div>div>div:last-child div>div:first-child',
-                        blockSelector: '  div[data-testid="incoming_group"] > div:last-child > div:first-child[class] > div >div',
+                        blockSelector: '  div[data-testid="incoming_group"] > div:last-child > div:first-child[class]>div>div',
                         inlineNears: true,
                         blockNears: false,
                     },
                     {
                         isComponent: "name",
                         block: 'div[data-pagelet="root"] div[data-pagelet="ChatTab"] div[data-visualcompletion="ignore-dynamic"] div[role="region"]>div>div>div>div:last-child>div>div:first-child',
-                        blockSelector: ' div[data-testid="outgoing_group"]>div[class]',
+                        blockSelector: ' div[data-testid="outgoing_group"]>div[class]>div>div>div',
                         inlineNears: true,
                         blockNears: false,
                     },
