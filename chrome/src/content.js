a63a4582d893b08c11de619f4dfe6f1b5a475d63
pdudctnguyen <pdudctnguyen@gmail.com>
2020-07-19 10:00:20 +0700

    Auto commit with build

--- chrome/src/content.js
+++ chrome/src/content.js
@@ -1368,6 +1368,24 @@ function main() {
 window.addEventListener('locationchange', function(){
     console.log('location changed!', window.location);
 })
+/* These are the modifications: */
+history.pushState = ( f => function pushState(){
+    var ret = f.apply(this, arguments);
+    window.dispatchEvent(new Event('pushstate'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+})(history.pushState);
+
+history.replaceState = ( f => function replaceState(){
+    var ret = f.apply(this, arguments);
+    window.dispatchEvent(new Event('replacestate'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+})(history.replaceState);
+
+window.addEventListener('popstate',()=>{
+    window.dispatchEvent(new Event('locationchange'))
+});
 
 main();
 chrome.runtime.onMessage.addListener(
