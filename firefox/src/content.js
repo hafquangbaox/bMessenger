06bccb4a859e59fa853fd69f2d86604b2d6690bd
pdudctnguyen <pdudctnguyen@gmail.com>
2020-07-19 09:34:31 +0700

    Auto commit with build

--- firefox/src/content.js
+++ firefox/src/content.js
@@ -41,6 +41,20 @@ var selector = {
                 },
 
                     /*Version 2 start*/
+                    {
+                        isComponent: 'name',
+                        selector: 'ul>li[role="row"]>div>a>div>div>div:first-of-type>span',
+                        block: 'div.uiScrollableArea>div.uiScrollableAreaWrap>div.uiScrollableAreaBody>div.uiScrollableAreaContent>div[role="navigation"]',
+                        inlineNears: true,
+                        blockNears: false,
+                    }, {
+                        isComponent: 'message',
+                        selector: 'ul>li[role="row"]>div>a>div>div>div:last-child>span',
+                        block: 'div.uiScrollableArea>div.uiScrollableAreaWrap>div.uiScrollableAreaBody>div.uiScrollableAreaContent>div[role="navigation"]',
+                        inlineNears: true,
+                        blockNears: false,
+                    },
+
                     /*Version 2 end*/
                 ],
                 'blur25': [{
